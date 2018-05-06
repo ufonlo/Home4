e5b164cca5fce21dba67d5bb2357e9f37c087d88
Hennadii Bohovuk <socufo@gmail.com>
2018-05-06 22:03:06 +0300

    Second

--- src/homeWork/firstHomework.java
+++ src/homeWork/firstHomework.java
@@ -2,7 +2,12 @@ package homeWork;
 
 public class firstHomework {
 	public static void main (String []args) {
-		System.out.println("Lets Start");
-	}
+		int x = 10;
 
+	      while( x < 20 ) {
+	         System.out.print("value of x : " + x );
+	         x++;
+	         System.out.print("\n");
+	}
+	}
 }
